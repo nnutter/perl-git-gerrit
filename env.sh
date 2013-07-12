f2c570f891ed8ff69733d63dcbdfccbc45a21a30
Nathan Nutter <iam@nnutter.com>
2013-07-12 10:05:20 -0500

    add env.sh

---
+++ env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+
+OWD="$(pwd)"
+
+cd "$(git rev-parse --show-cdup)"
+
+git submodule update --init
+git submodule foreach 'git checkout master'
+git submodule foreach 'git pull --ff-only'
+
+for D in $(git submodule foreach --quiet pwd); do
+    if [ -d "$D/lib" ]; then
+        PERL5LIB="$D/lib:$PERL5LIB"
+    fi
+done
+PERL5LIB="$(pwd)/lib:$PERL5LIB"
+export PERL5LIB
+
+cd "$OWD"
+
+set +o nounset
